1db12a8a7674854cb5cd763cf16338b2d51d9398
sakariyadisha17 <sakariyadisha17@gmail.com>
2026-02-14 15:06:07 +0530

    Update appointment.blade.php

--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 
-    <!-- Custom CSS -->
+    <!-- Custom CSS test-->
     <style>
         body {
             background: linear-gradient(135deg, #74b9ff, #81ecec);
